a7d1f2a57e1af99fa30e75f9bd39c2e06d510232
Riki <riki@Rikis-MBP.fios-router.home>
2020-05-23 13:31:46 -0400

    Setting up client htmls

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-//Hi this is nilly
-//Hi again test
-console.log("hello world!");
+const express = require('express');
+const app = express();
+const server = https.createServer({ key: key, cert: cert }, app);
+// const path = require('path');
+const router = express.Router();
+
+app.use(express.static(__dirname + "/public"));
+app.use(router);
+app.route("/");
+
+server.listen(3001, () => {
+  console.log("listening on 3001");
+});
+
+
+//root html file
+app.get("/", function (req, res) {
+  res.sendFile("root");
+});
