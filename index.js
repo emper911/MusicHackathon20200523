98c3fa2bf5bb5c7b75cefb00d3f74ea612a2926d
Riki <riki@Rikis-MBP.fios-router.home>
2020-05-23 13:33:33 -0400

    Server is now working

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const server = https.createServer({ key: key, cert: cert }, app);
+// const server = https.createServer({ key: key, cert: cert }, app);
 // const path = require('path');
 const router = express.Router();
 
@@ -8,8 +8,8 @@ app.use(express.static(__dirname + "/public"));
 app.use(router);
 app.route("/");
 
-server.listen(3001, () => {
-  console.log("listening on 3001");
+app.listen(3000, () => {
+  console.log("listening on 3000");
 });
 
 
